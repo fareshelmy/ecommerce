cd2e4f591dea01a51e57e3a0708b6fa5a820a62f
fares <fares.helmy93@gmail.com>
2019-03-19 18:50:09 +0200

    Database Configuration 2

--- ecommerce/src/main/java/model/dao/interfaces/UserDAO.java
+++ ecommerce/src/main/java/model/dao/interfaces/UserDAO.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package model.dao;
+package model.dao.interfaces;
 
 /**
  *
  * @author FARES-LAP
  */
-public class UserDAO {
+public interface UserDAO {
     
 }
