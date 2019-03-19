24291736e21f721d0163b41895203a2d98c63898
Mohamedj23 <mohamedj239@gmail.com>
2019-03-19 21:48:10 +0200

    Create MySessionFactory.java

---
+++ ecommerce/src/main/java/model/entity/MySessionFactory.java
@@ -0,0 +1,28 @@
+package model.entity;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author Mohamed Jamal
+ */
+public class MySessionFactory {
+
+    private static SessionFactory sessionFactory;
+
+    private MySessionFactory() {
+    }
+
+    public static Session getHibernateSession() {
+        if (sessionFactory == null) {
+            synchronized (MySessionFactory.class) {
+                if (sessionFactory == null) {
+                    sessionFactory = new Configuration().configure().buildSessionFactory();
+                }
+            }
+        }
+        return sessionFactory.openSession();
+    }
+}
