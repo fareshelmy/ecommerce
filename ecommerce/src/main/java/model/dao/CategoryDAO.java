b59267ad88f2dc282cb7b331d78d4a3b4891e60e
Mohamedj23 <mohamedj239@gmail.com>
2019-03-23 11:46:18 +0200

    Repairing Structure

---
+++ ecommerce/src/main/java/model/dao/CategoryDAO.java
@@ -0,0 +1,89 @@
+package model.dao;
+
+import java.util.List;
+import model.entity.Category;
+import model.util.HibernateUtil;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.ProjectionList;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+
+/**
+ *
+ * @author Zainab
+ */
+public class CategoryDAO implements DAO<Category>{
+    private  Session session;
+    public CategoryDAO(){
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
+    }
+    private void getSession(){
+        if(!session.isOpen())
+            session = HibernateUtil.getSessionFactory().getCurrentSession();
+    }
+    @Override
+    public void persist(Category category) {
+        getSession();
+        session.getTransaction().begin();
+        session.persist(category);
+        session.getTransaction().commit();
+    }
+
+    @Override
+    public void update(Category category) {
+        getSession();
+        session.beginTransaction();
+        session.saveOrUpdate(category);
+        session.getTransaction().commit();
+    }
+
+    @Override
+    public void delete(Category category) {
+        getSession();
+        session.getTransaction().begin();
+        session.delete(category);
+        session.getTransaction().commit();
+    }
+
+    @Override
+    public Category retrieve(Object primaryKey) {
+        getSession();
+        int key = (int) primaryKey;
+        session.getTransaction().begin();
+        Category category = (Category) session.get(Category.class, key);
+        session.getTransaction().commit();
+        return category;
+    }
+
+    @Override
+    public List<Category> getByColumnNames(Object primaryKey, List<String> columnNames) {
+        getSession();
+        int key = (int) primaryKey;
+        session.getTransaction().begin();
+        Criteria categoryCriteria = session.createCriteria(model.entity.Category.class);
+        ProjectionList selectedColumns = Projections.projectionList();
+         for(int i = 0; i<columnNames.size(); i++){
+            selectedColumns.add(Projections.property(columnNames.get(i)));
+        }
+        categoryCriteria = categoryCriteria.setProjection(selectedColumns);
+        categoryCriteria = categoryCriteria.add(Restrictions.idEq(key));
+        List subsetCategory = categoryCriteria.list();
+        session.getTransaction().commit();
+        return subsetCategory;
+    }
+
+    @Override
+    public List<Category> getAll(Object cat) {
+        getSession();
+        Category category = (Category) cat;
+        session.getTransaction().begin();
+        Criteria categoryCriteria = session.createCriteria(model.entity.User.class);
+        categoryCriteria = categoryCriteria.add(Restrictions.eq("id", category.getId()));
+        List<Category> allUser = categoryCriteria.list();
+        session.getTransaction().commit();
+        return allUser;
+
+    }
+   
+}
---
+++ ecommerce/src/main/java/model/dao/OrderItemDAO.java
@@ -0,0 +1,166 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.dao;
+
+import java.util.List;
+import model.entity.OrderItem;
+import model.entity.OrderItemId;
+import model.util.HibernateUtil;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.ProjectionList;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+
+/**
+ *
+ * @author Mayada Khaled
+ */
+public class OrderItemDAO implements DAO<OrderItem> {
+    private Session session;
+    public OrderItemDAO(){
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
+    }
+    private void getSession(){
+        if(!session.isOpen())
+            session = HibernateUtil.getSessionFactory().getCurrentSession();
+    }
+    @Override
+    public void persist(OrderItem orderItem) {
+        getSession();
+        session.getTransaction().begin();
+        session.persist(orderItem);
+        session.getTransaction().commit();
+    }
+
+    @Override
+    public void update(OrderItem orderItem) {
+        getSession();
+        session.getTransaction().begin();
+        session.saveOrUpdate(orderItem);
+        session.getTransaction().commit();
+    }
+
+    @Override
+    public void delete(OrderItem orderItem) {
+        getSession();
+        session.getTransaction().begin();
+        session.delete(orderItem);
+        session.getTransaction().commit();
+    }
+
+    @Override
+    public OrderItem retrieve(Object primaryKey) {
+        getSession();
+        OrderItemId orderItemId = (OrderItemId) primaryKey;
+        session.getTransaction().begin();
+        OrderItem orderItem = (OrderItem) session.get(OrderItem.class, orderItemId);
+        session.getTransaction().commit();
+        return orderItem;
+    }
+
+    @Override
+    public List<OrderItem> getByColumnNames(Object primaryKey, List<String> columnNames) {
+        getSession();
+        OrderItemId key = (OrderItemId) primaryKey;
+        session.getTransaction().begin();
+        Criteria orderItemCriteria = session.createCriteria(model.entity.OrderItem.class);
+        ProjectionList selectedColumns = Projections.projectionList();
+         for(int i = 0; i<columnNames.size(); i++){
+            selectedColumns.add(Projections.property(columnNames.get(i)));
+        }
+        orderItemCriteria = orderItemCriteria.setProjection(selectedColumns);
+        orderItemCriteria = orderItemCriteria.add(Restrictions.idEq(key));
+        List subsetOrderItem = orderItemCriteria.list();
+        session.getTransaction().commit();
+        return subsetOrderItem;
+    }
+
+    @Override
+    public List<OrderItem> getAll(Object orderitem) {
+         getSession();
+        OrderItem orderItem = (OrderItem) orderitem;
+        session.getTransaction().begin();
+        Criteria orderItemCriteria = session.createCriteria(model.entity.OrderItem.class);
+        orderItemCriteria = orderItemCriteria.add(Restrictions.idEq(orderItem.getId()));
+        List<OrderItem> allUser = orderItemCriteria.list();
+        session.getTransaction().commit();
+        return allUser;
+    }
+
+}
+
+
+
+
+
+
+    /*implements DAO<OrderItem>
+    Session session;
+    @Override
+    public void delete(OrderItem orderItem) {
+        
+        session = MySessionFactory.getHibernateSession();
+        session.beginTransaction();
+
+        Query query = session.createQuery("delete OrderItem where id = :ID");
+        query.setParameter("ID", orderItem.getId());
+
+        query.executeUpdate();
+
+        session.getTransaction().commit();
+         MySessionFactory.closeSession(session);
+
+    }
+
+    @Override
+    public OrderItem retrieve(Object primaryKey) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<OrderItem> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<OrderItem> getByColumnNames(Object primaryKey, List<String> columnNames) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void persist(OrderItem orderItem) {
+        System.out.println(orderItem.getId()+" the new object that is going to be persisted ");
+        session = MySessionFactory.getHibernateSession();
+        session.beginTransaction();
+        session.persist(orderItem);
+        System.out.println("persisted ");
+        session.getTransaction().commit();
+        MySessionFactory.closeSession(session);
+    }
+
+    @Override
+    public void update(OrderItem orderItem) {
+        session = MySessionFactory.getHibernateSession();
+        session.beginTransaction();
+        model.entity.OrderItem newOrderItem;
+        System.out.println(orderItem.getQuantity() +" old value");
+        Criteria query = session.createCriteria(model.entity.OrderItem.class)
+                .add(Restrictions.eq("id", orderItem.getId()));
+        newOrderItem = (model.entity.OrderItem) query.uniqueResult();
+
+        newOrderItem.setOrder(orderItem.getOrder());
+        newOrderItem.setProduct(orderItem.getProduct());
+        newOrderItem.setQuantity(orderItem.getQuantity());
+        newOrderItem.setTotal(orderItem.getTotal());
+
+        session.saveOrUpdate(newOrderItem);
+        
+        session.getTransaction().commit();
+        System.out.println(orderItem.getQuantity() +" new value");
+        MySessionFactory.closeSession(session);
+    }
+*/
